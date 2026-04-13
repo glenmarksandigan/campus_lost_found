1f1b7b539cba56c19323b9dc80a2f316b2e806dc
glenmarksandigan <sandidanglenmark1@gmail.comm>
2026-04-13 19:53:56 +0800

    db.php updated

--- web/db.php
+++ web/db.php
@@ -1,21 +1,13 @@
-
 <?php
-$host = 'localhost';
-$dbname = 'campus_lost_found';
-$username = 'root';
-$password = '';
+$host = 'mysql-2b818467-sandiganglenmark1-355b.e.aivencloud.com';
+$port = '24590';
+$dbname = 'defaultdb';
+$username = 'avnadmin';
+$password = '<redacted>';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error: " . $e->getMessage());
-}
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
-// some legacy scripts use a mysqli connection named $conn; create it here as well
-$conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("MySQL connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-$conn->set_charset('utf8mb4');
 ?>
