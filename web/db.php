093402d400a85a14a51d7529face749595131ec8
glenmarksandigan <sandidanglenmark1@gmail.comm>
2026-04-13 22:55:50 +0800

    fix: pdo ssl compatibility

--- web/db.php
+++ web/db.php
@@ -23,7 +23,7 @@ try {
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
-        PDO::MYSQL_ATTR_SSL_CA       => true,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ];
     $pdo = new PDO($dsn, $username, $password, $options);
 } catch (\PDOException $e) {
